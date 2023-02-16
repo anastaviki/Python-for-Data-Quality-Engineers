d0aa06d90505054c4d3b69f0a406938eb565fcd6
anastaviki <64853158+anastaviki@users.noreply.github.com>
2023-02-16 12:48:27 +0100

    3d HW reworked

--- Strings Object Func/main.py
+++ Strings Object Func/main.py
@@ -34,21 +34,32 @@ start_text = """homEwork:
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
 
-print("Text: \n", start_text)
-sentences = start_text.split('.')  # split to sentences
-for sentence in sentences:  # loop for every sentence
-    if not sentence:  # check if smth exists in sentence
-        continue
-    sentence = sentence.strip()  # remove whitespaces in begin and in the end of sentence
-    sentence_new = sentence.capitalize()  # first letter in sentence in upper case
-    start_text = start_text.replace(sentence, sentence_new)  # generate new sentence
+# capitalise any text
+def text_capitalise(p_start_text):
+    sentences = p_start_text.split('.')  # split to sentences
+    for sentence in sentences:  # loop for every sentence
+        if not sentence:  # check if smth exists in sentence
+            continue
+        sentence = sentence.strip()  # remove whitespaces in begin and in the end of sentence
+        sentence_new = sentence.capitalize()  # first letter in sentence in upper case
+        p_start_text = p_start_text.replace(sentence, sentence_new)  # generate new sentence
+    return p_start_text
 
-last_words = re.findall(r'\b\w+\.', start_text)  # find all last words in every sentence
-last_sent = "".join(last_words).replace(".", " ").strip().capitalize() + '.'  # generate last sentence
+
+# generate sentence from words in text witch is match with regular expression
+def generate_sentence_reg (p_start_text, p_reg):
+    last_words = re.findall(p_reg, p_start_text)  # find all last words in every sentence
+    r_last_sent = "".join(last_words).replace(".", " ").strip().capitalize() + '.'  # generate last sentence
+    return r_last_sent
+
+
+print("Text: \n", start_text)
+start_text = text_capitalise(start_text)  # capitalise text
 new_text = start_text
+last_sent = generate_sentence_reg(start_text, r'\b\w+\.')  # generate new sentence
 # add last sentence
 new_text = new_text.replace("END OF this Paragraph.".lower(), "END OF this Paragraph! ".lower()+last_sent)
 new_text = new_text.replace(" iz ", " is ")  # replace incorrect iz
-print("Text after all changes: \n",new_text)
+print("Text after all changes: \n", new_text)
 res = len(re.findall(r"\s", start_text))  # find all whitespaces
 print("Number of whitespaces: ", res)
