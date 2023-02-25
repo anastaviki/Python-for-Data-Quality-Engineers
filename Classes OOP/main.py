c3fd67bf7ccc47cb29eee8affd57f524d0eb2659
anastaviki <64853158+anastaviki@users.noreply.github.com>
2023-02-25 20:17:25 +0100

    Gui change classs

--- Classes OOP/main.py
+++ Classes OOP/main.py
@@ -2,27 +2,29 @@ from tkinter import *
 from tkinter.ttk import Combobox
 
 class WindowMain( ):
-    def __init__(self, p_title, ):
+    def __init__(self, p_title, p_lable):
         self.title = p_title
         self.window = Tk()
+        self.window.geometry('400x250')
+        self.lbl = Label(self.window, text=p_lable)
 
 class Window (WindowMain):
 
-    def __init__(self, p_title ):
-        WindowMain.__init__(self, p_title)
-        self.lbl = Label(self.window, text="Select input type")
+    def __init__(self, p_title,p_lable =""  ):
+        WindowMain.__init__(self, p_title,p_lable )
+
         self.combo = Combobox(self.window)
         self.btn = Button(self.window, text="Add", command=self.clicked)
         self.input_type = 0
 
     def show_window(self):
-        self.window.geometry('400x250')
+
         self.window.title(self.title)
         self.lbl.grid(column=0, row=0)
         self.combo['values'] = ("News", "Privat ad", "Recipe")
         self.combo.current(0)  # установите вариант по умолчанию
         self.combo.grid(column=1, row=0)
-        self.btn.grid(column=0, row=2)
+        self.btn.grid(column=2, row=0)
         self.window.mainloop()
     def clicked(self):
         x = self.combo.get()
@@ -30,6 +32,9 @@ class Window (WindowMain):
         self.window.destroy()
 
 
+window = Window("Add News", "Select Type of New")
+window.show_window()
+print (window.input_type)
 window = Window("Add News")
 window.show_window()
 print (window.input_type)
